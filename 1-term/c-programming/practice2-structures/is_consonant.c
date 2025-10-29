7689b2191fa6cfb80aa68b70fa4a6df451c9cbae
SachsenspieltCoding <coding@sachsenspielt.de>
2025-10-29 15:25:26 +0100

    added consonant char checker

---
+++ 1-term/c-programming/practice2-structures/is_consonant.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main() {
+    char input;
+
+    printf("Is this letter a consonant?");
+    scanf("%c", &input);
+
+    switch (toupper(input)) {   
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("%c is not a consonant", input);
+            break;
+
+        default:
+            printf("%c is a consonant", input);
+            break;
+    }
+
+    return 0;
+}
--- 1-term/c-programming/practice2-structures/random_game.c
+++ 1-term/c-programming/practice2-structures/random_game.c
@@ -1,7 +1,3 @@
-//
-// Created by finnmargraf on 10/29/25.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
