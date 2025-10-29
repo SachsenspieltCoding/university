835648e666b68dc4ba986808e66f022babf659ee
SachsenspieltCoding <coding@sachsenspielt.de>
2025-10-29 15:17:06 +0100

    added leap year calculator

---
+++ 1-term/c-programming/practice2-structures/leap_year.c
@@ -0,0 +1,20 @@
+//
+// Created by finnmargraf on 10/29/25.
+//
+
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("Is this year a leap year?\n");
+    scanf("%d", &year);
+
+    if (!((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))) {
+        printf("Das Jahr %d ist KEIN Schaltjahr!", year);
+        return 0;
+    }
+
+    printf("Das Jahr %d ist ein Schaltjahr!", year);
+    return 0;
+}
